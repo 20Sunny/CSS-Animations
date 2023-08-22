ea9382e00f2f6d324c60b01030454c7c8f4110b7
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2023-08-22 20:21:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,4 @@ let myEmojis = [
 let urlAnimate = () => {
     window.location.hash = myEmojis[Math.floor((Date.now() / 100) % myEmojis.length)];
     setTimeout(urlAnimate, 300);
-};
-urlAnimate(); // Start the animation
+};urlAnimate(); // Start the animation
